56c8fa9244400149278a2b3b64afe0e8128d20f1
Roman Gebrehiwot <rttgg1@gmail.com>
2019-08-27 13:39:47 -0700

    finished

--- codefellowship/src/main/java/com/gebrehiwot/codefellowship/CodefellowshipApplication.java
+++ codefellowship/src/main/java/com/gebrehiwot/codefellowship/CodefellowshipApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CodefellowshipApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CodefellowshipApplication.class, args);
 	}
+
 
 }
